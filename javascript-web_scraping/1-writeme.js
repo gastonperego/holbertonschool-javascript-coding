e698f7e16e9a0753d6ae197f7cf63e6a16f1d2ae
gastonperego <gastonperego@gmail.com>
2024-01-22 22:05:43 +0000

    fix: semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const data = new Uint8Array(Buffer.from(process.argv[3]))
-fs.writeFile(process.argv[2], data, (err) => {
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
   if (err) throw err;
 });
-
-
