82d7368318b72b27e0e09c67e08899726612ff11
gastonperego <gastonperego@gmail.com>
2024-02-03 19:15:55 +0000

    fix: error

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express()
 
-app.use('/', (req, res) => {
-    res.send('Hello Holberton School!')
-})
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+  });
+
+app.use((req, res) => {
+  res.status(404).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Error</title>
+    </head>
+    <body>
+      <pre>Cannot GET ${req.url}</pre>
+    </body>
+    </html>
+  `);
+});
 
 app.listen(1245);
 
