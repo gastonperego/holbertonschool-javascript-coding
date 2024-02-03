ccaa316006c1dce44fb28bf32b80c0af7ff8a881
gastonperego <gastonperego@gmail.com>
2024-02-03 17:06:19 +0000

    feat: task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n');
+    const finalArray = lines.filter((x) => x.length > 0);
+    console.log(`Number of students: ${finalArray.length - 1}`);
+    let field1 = 0;
+    let field2 = 0;
+    const array1 = [];
+    const array2 = [];
+    for (const line of finalArray) {
+      const data = line.split(',');
+      if (data[3] === 'CS') {
+        array1.push(data[0]);
+        field1 += 1;
+      }
+      if (data[3] === 'SWE') {
+        array2.push(data[0]);
+        field2 += 1;
+      }
+    }
+    console.log(`Number of students in CS: ${field1}. List: ${array1.join(', ')}`);
+    console.log(`Number of students in SWE: ${field2}. List: ${array2.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
